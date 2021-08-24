70e72bbd10d1190c394cdc0dfff539cc3c326423
Milind-dev <milinddev1011@gmail.com>
2021-08-24 11:32:45 +0530

    git accept branch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
     return (
         <div>
-            <h1>Hello World!</h1>
+            <h1> hello git milind branch</h1>
         </div>
     )
 }
