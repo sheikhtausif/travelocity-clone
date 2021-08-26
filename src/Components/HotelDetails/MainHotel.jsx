53625a4715209409884a1316cc2edb8fabac0280
Mohd Tausif <mohammadtausif14@gmail.com>
2021-08-26 23:42:10 +0530

    font weight fix

--- src/Components/HotelDetails/MainHotel.jsx
+++ src/Components/HotelDetails/MainHotel.jsx
@@ -2,22 +2,41 @@ import React from 'react'
 import styles from './styles/mainHotel.module.css'
 import Ads from './Ads'
 import HotelDetails from './HotelDetails'
+import { useState, useEffect } from 'react'
+import { CircularProgress } from '@material-ui/core'
 
 const MainHotel = () => {
+
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(true)
+        }, 1000)
+    }, [])
+
     return (
-        <div className={styles.container}>
-            <div className={styles.mainBox}>
-                <div className={styles.left}>
-                    <HotelDetails />
-                </div>
-                <div className={styles.right}>
-                    <Ads />
-                    <Ads />
-                    <Ads />
-                    <Ads style={{ position: 'fixed', top: '50', }} />
+        <>
+            {loading ? <div className={styles.container} >
+                <div className={styles.mainBox}>
+                    <div className={styles.left}>
+                        <HotelDetails />
+                    </div>
+                    <div className={styles.right}>
+                        <Ads />
+                        <Ads />
+                        <Ads />
+                        <Ads />
+                        <Ads style={{ position: 'fixed', top: '50', }} />
+                    </div>
                 </div>
             </div>
-        </div>
+                : <div>
+                    <CircularProgress />
+                </div>
+
+            }
+        </>
     )
 }
 
