fd15c8365ded3ec41022b80020b525e19986728e
Chandan gupta <chandan1499>
2021-08-26 10:57:53 +0530

    home page with hotel done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { HomePage } from './components/HomePage/Home'
 
 const App = () => {
     return (
         <div>
-            <h1>Hello World!</h1>
+            {/* <StaticDateRangePickerDemo /> */}
+            <HomePage />
         </div>
     )
 }
