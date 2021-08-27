d0f78cac827b3d6608f4eaeb7e2a6fcf88faf366
Chandan gupta <chandan1499>
2021-08-28 00:58:32 +0530

    routing done with home page and hotels

--- src/Components/HomePage/Slider.jsx
+++ src/Components/HomePage/Slider.jsx
@@ -420,7 +420,7 @@ export default function ScrollableTabsButtonForce() {
                             </div>
                         </div>
                     </div>
-                    <div style={{width: '28%'}} className={`${styles.hotelBtns} ${styles.hoteldateBtn}`}>
+                    <div style={{ width: '28%' }} className={`${styles.hotelBtns} ${styles.hoteldateBtn}`}>
                         <EventIcon className={styles.Icon} />
                         <div className={styles.HeadingGoingto}>
                             <div className={styles.checkInHeading}>Departing</div>
@@ -438,7 +438,45 @@ export default function ScrollableTabsButtonForce() {
             </TabPanel>
             <TabPanel value={value} index={2}>
                 <div className={styles.hotelContainer}>
-                    <div className={`${styles.hotelBtns} ${styles.hotelGoingToBtn}`} onClick={(e) => { handleOpenLocation(e) }}>
+                    <div style={{ width: "32%" }} className={`${styles.hotelBtns} ${styles.hotelGoingToBtn}`} onClick={(e) => { handleFlightLeavingOpenSearchPopup(e) }}>
+                        <LocationOnIcon className={styles.Icon} />
+                        <div className={styles.HeadingGoingto}>
+                            {(flightLeavingVal === "") ? "Leaving from" : <div><div className={styles.checkInHeading}>Leaving from</div>
+                                <div className={styles.checkinDate}>{flightLeavingVal}</div></div>}
+                        </div>
+                        {/* -------Searching pop up bar------- */}
+                        <div className={styles.searchBox} style={{ display: `${flightLeavingSearchPopup}` }}>
+                            <input ref={searchRef} value={query} onChange={(e) => { setQuery(e.target.value) }} type="text" className={styles.searchInput} placeholder="Where are you going?" />
+                            <div className={styles.searchResultHight}>
+                                <div className={styles.SearchResult}>
+                                    <div className={styles.SearchResultIndividual} onClick={(e) => { handleFlightLeavingCloseSearchPopup("Mumbai", e) }}>
+                                        <RestoreIcon className={styles.searchIcon} />
+                                        <div className={styles.SearchResultsMapping}>
+                                            <strong>Mumbai</strong>
+                                            <div>IN</div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                {
+                                    searchQueryResult.map((data) => {
+                                        return <div key={uuid()} className={styles.SearchResult}>
+                                            <div className={styles.SearchResultIndividual} onClick={(e) => { handleFlightLeavingCloseSearchPopup(`${data.name}`, e) }}>
+                                                <LocationOnIcon className={styles.searchIcon} />
+                                                <div className={styles.SearchResultsMapping}>
+                                                    <strong>{data.name}</strong>
+                                                    <div>{data.country}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{ width: "32%" }} className={`${styles.hotelBtns} ${styles.hotelGoingToBtn}`} onClick={(e) => { handleOpenLocation(e) }}>
+                        <div className={styles.swapIconDiv} onClick={(e) => { e.stopPropagation(); handleSwap(); }}><SwapHorizSharpIcon className={styles.swapIcon} /></div>
+
                         <LocationOnIcon className={styles.Icon} />
                         <div className={styles.HeadingGoingto}>
                             {(goingToVal === "") ? "Going to" : <div><div className={styles.checkInHeading}>Going to</div>
@@ -449,10 +487,10 @@ export default function ScrollableTabsButtonForce() {
                             <input ref={searchRef} value={query} onChange={(e) => { setQuery(e.target.value) }} type="text" className={styles.searchInput} placeholder="Where are you going?" />
                             <div className={styles.searchResultHight}>
                                 <div className={styles.SearchResult}>
-                                    <div className={styles.SearchResultIndividual} onClick={(e) => { handleCloseLocation("Goa", e) }}>
+                                    <div className={styles.SearchResultIndividual} onClick={(e) => { handleCloseLocation("Mumbai", e) }}>
                                         <RestoreIcon className={styles.searchIcon} />
                                         <div className={styles.SearchResultsMapping}>
-                                            <strong>Goa</strong>
+                                            <strong>Mumbai</strong>
                                             <div>IN</div>
                                         </div>
                                     </div>
@@ -474,75 +512,15 @@ export default function ScrollableTabsButtonForce() {
                             </div>
                         </div>
                     </div>
-                    <div className={`${styles.hotelBtns} ${styles.hoteldateBtn}`}>
+                    <div style={{ width: '28%' }} className={`${styles.hotelBtns} ${styles.hoteldateBtn}`}>
                         <EventIcon className={styles.Icon} />
                         <div className={styles.HeadingGoingto}>
-                            <div className={styles.checkInHeading}>Check-in</div>
+                            <div className={styles.checkInHeading}>Departing</div>
                             <div className={styles.checkinDate}>{`${startDate.getDate()} ${monthNames[startDate.getMonth()]} ${sort_year(startDate)}`}</div>
                             <MuiPickersUtilsProvider utils={DateFnsUtils}>
                                 <MaterialUIPickers props={{ handleStartDate }} type={"checkIn"} />
                             </MuiPickersUtilsProvider>
 
-                        </div>
-                    </div>
-                    <div className={`${styles.hotelBtns} ${styles.hoteldateBtn}`}>
-                        <EventIcon className={styles.Icon} />
-                        <div className={styles.HeadingGoingto}>
-                            <div className={styles.checkInHeading}>Check-out</div>
-                            <div className={styles.checkinDate}>{`${endDate.getDate()} ${monthNames[endDate.getMonth()]} ${sort_year(endDate)}`}</div>
-                        </div>
-                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                            <MaterialUIPickers props={{ handleEndDate }} startDate={startDate} type={"checkOut"} />
-                        </MuiPickersUtilsProvider>
-                    </div>
-                    <div className={`${styles.hotelBtns} ${styles.hotelTraverls}`} onClick={(e) => { e.stopPropagation(); setTrevelersPopupOpen("inline") }}>
-                        <PersonIcon className={styles.Icon} />
-                        <div className={styles.HeadingGoingto}>
-                            <div className={styles.checkInHeading}>Trevelers</div>
-                            <div className={styles.checkinDate}>{trevelersRoomCnt} room, {AdultsTrevelers} trevelers</div>
-                        </div>
-                        {/* ---------slecting traverlers and room--------- */}
-                        <div className={styles.traverlersPopup} style={{ display: `${trevelersPopupOpen}` }}>
-                            <div>
-                                <h3 className={styles.travelersPopupHeading}>Travelers</h3>
-                            </div>
-                            <div className={styles.trevelersFlexContainer}>
-                                <h3 className={styles.treveleresAdultSelector}>Room (Max 4 Adults/per room)</h3>
-                                <div className={styles.trevelersFlexChild}>
-                                    <div className={styles.increseDecreaseIcon} style={{ opacity: `${(trevelersRoomCnt === 1) ? 0.5 : 1}` }} onClick={(e) => { handleAdulTrevelersRoomCnt(e, -1) }}>
-                                        <RemoveIcon className={styles.incDecIcon} />
-                                    </div>
-                                    <h3>{trevelersRoomCnt}</h3>
-                                    <div className={styles.increseDecreaseIcon} style={{ opacity: `${(trevelersRoomCnt === 5) ? 0.5 : 1}` }} onClick={(e) => { handleAdulTrevelersRoomCnt(e, 1) }}>
-                                        <AddIcon className={styles.incDecIcon} />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={styles.trevelersFlexContainer}>
-                                <h3 className={styles.treveleresAdultSelector}>Adults</h3>
-                                <div className={styles.trevelersFlexChild}>
-                                    <div className={styles.increseDecreaseIcon} style={{ opacity: `${(AdultsTrevelers === 1) ? 0.5 : 1}` }} onClick={(e) => { handleAdulTrevelersCnt(e, -1) }}>
-                                        <RemoveIcon className={styles.incDecIcon} />
-                                    </div>
-                                    <h3>{AdultsTrevelers}</h3>
-                                    <div className={styles.increseDecreaseIcon} onClick={(e) => { handleAdulTrevelersCnt(e, 1) }}>
-                                        <AddIcon className={styles.incDecIcon} />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={styles.trevelersFlexContainer}>
-                                <h3 className={styles.treveleresAdultSelector}>Children (Ages 5 to 12)</h3>
-                                <div className={styles.trevelersFlexChild}>
-                                    <div className={styles.increseDecreaseIcon} style={{ opacity: `${(childrenCnt === 0) ? 0.5 : 1}` }} onClick={(e) => { handleChildrenCnt(e, -1) }}>
-                                        <RemoveIcon className={styles.incDecIcon} />
-                                    </div>
-                                    <h3>{childrenCnt}</h3>
-                                    <div className={styles.increseDecreaseIcon} onClick={(e) => { handleChildrenCnt(e, 1) }}>
-                                        <AddIcon className={styles.incDecIcon} />
-                                    </div>
-                                </div>
-                            </div>
-                            <div style={{ textAlign: 'left', paddingLeft: "10px" }}><button className={styles.trevelersSelectingDone} onClick={(e) => { e.stopPropagation(); setTrevelersPopupOpen("none") }}>Done</button></div>
                         </div>
                     </div>
                 </div>
