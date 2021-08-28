1bed18a43bdae956798f8161009d63fd3e837d79
Chandan gupta <chandan1499>
2021-08-28 21:34:41 +0530

    sign in and sign up with navbar and scrolling issue fixed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 export function Navbar() {
+    const userName = useSelector((state) => state.userName);
+
     return (
         <div className={styles.container}>
             <nav className={styles.nav}>
@@ -15,7 +19,8 @@ export function Navbar() {
                             <li><a className={styles.travelNavLink} href="#">List your property</a></li>
                             <li><a className={styles.travelNavLink} href="#">Support</a></li>
                             <li><a className={styles.travelNavLink} href="#">Trips</a></li>
-                            <li><Link className={styles.travelNavLink} to="/signIn">Sign In</Link></li>
+                            <li><Link className={styles.travelNavLink} to="/signIn">{(userName === "") ? "Sign In" : userName}</Link></li>
+                            <li><Link className={styles.travelNavLink} style={{display: `${(userName === "") ? "none" : "inline"}`}} to="/">Sign out</Link></li>
                         </ul>
                     </div>
                 </div>
