ac004b477d839519d4e27b1375d83108ab81ff88
Chandan gupta <chandan1499>
2021-08-28 13:43:02 +0530

    marging and padding fixed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,5 @@
 import styles from "./Footer.module.css";
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 
 
 export function Footer() {
