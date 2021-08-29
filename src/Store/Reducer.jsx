6c79692d86b8ba1f399120be019a43186f225870
Chandan gupta <chandan1499>
2021-08-29 09:34:49 +0530

    sign in not changes on refreshing now

--- src/Store/Reducer.jsx
+++ src/Store/Reducer.jsx
@@ -1,13 +1,15 @@
+import { getData, setData } from "../LocalStorage/LocalStorage";
 import { LOGIN_FAILURE, LOGIN_SUCCESS, LOG_OUT } from "./ActionTypes";
 
 const initState = {
   login: false,
-  userName: "",
+  userName: getData("username") || "",
 };
 
 export const Authreducer = (state = initState, action) => {
   switch (action.type) {
     case LOGIN_SUCCESS:
+      setData("username", action.payload);
       return {
         ...state,
         login: true,
@@ -20,6 +22,7 @@ export const Authreducer = (state = initState, action) => {
         login: false,
       };
     case LOG_OUT:
+      setData("username", "");
       return {
         ...state,
         login: false,
